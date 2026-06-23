9754c5aaf39ed967262001e175b9466434b51390
= <ryscai@icloud.com>
2026-06-23 13:35:44 +0100

    start of implementation

---
+++ src/app.rs
@@ -0,0 +1,45 @@
+use std::sync::Arc;
+
+use winit::{application::ApplicationHandler, window::Window};
+
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+#[cfg(target_arch = "wasm32")]
+use winit::platform::web::EventLoopExtWebSys;
+
+pub struct State {
+    window: Arc<Window>,
+}
+
+impl State {
+    pub async fn new(window: Arc<Window>) -> anyhow::Result<Self> {
+        Ok(Self { window })
+    }
+
+    pub fn resize(&mut self, _width: u32, _height: u32) {}
+
+    pub fn render(&mut self) {
+        self.window.request_redraw();
+    }
+}
+
+pub struct App {
+    #[cfg(target_arch = "wasm32")]
+    proxy: Option<winit::event_loop::EventLoopProxy<State>>,
+    state: Option<State>,
+}
+
+impl App {
+    pub fn new(#[cfg(target_arch = "wasm32")] event_loop: &EventLoop<State>) -> Self {
+        #[cfg(target_arch = "wasm32")]
+        let proxy = Some(event_loop.create_proxy());
+
+        Self {
+            state: None,
+            #[cfg(target_arch = "wasm32")]
+            proxy,
+        }
+    }
+}
+
+impl ApplicationHandler<State> for App {}
