c5e9b26536328e34e65b3a2f3d91772766243742
= <ryscai@icloud.com>
2026-06-23 19:00:16 +0100

    decided to remove web assembly for now

--- src/app.rs
+++ src/app.rs
@@ -24,22 +24,17 @@ impl State {
 }
 
 pub struct App {
-    #[cfg(target_arch = "wasm32")]
-    proxy: Option<winit::event_loop::EventLoopProxy<State>>,
     state: Option<State>,
 }
 
 impl App {
-    pub fn new(#[cfg(target_arch = "wasm32")] event_loop: &EventLoop<State>) -> Self {
-        #[cfg(target_arch = "wasm32")]
-        let proxy = Some(event_loop.create_proxy());
-
-        Self {
-            state: None,
-            #[cfg(target_arch = "wasm32")]
-            proxy,
-        }
+    pub fn new() -> Self {
+        Self { state: None }
     }
 }
 
-impl ApplicationHandler<State> for App {}
+impl ApplicationHandler<State> for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let mut window_attributes = Window::default_attributes();
+    }
+}
